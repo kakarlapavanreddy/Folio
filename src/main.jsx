145cb9817baa0cce344d17aa8a296ed42d979293
kakarlapavankumar <kakarlapavankumar@thestackly.com>
2026-07-06 10:21:16 +0530

    Fix Global.css import

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,8 @@ import ReactDOM from "react-dom/client";
 
 import App from "./App";
 
-import "./styles/global.css";
-import "./styles/colors.css";
+import "./styles/Global.css";
+import "./styles/Colors.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
