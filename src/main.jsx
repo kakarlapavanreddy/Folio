bd95337f93d76e8f25429523e65953a3e32f7db6
kakarlapavankumar <kakarlapavankumar@thestackly.com>
2026-07-06 10:40:01 +0530

    Fix CSS import case

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,8 @@ import ReactDOM from "react-dom/client";
 
 import App from "./App";
 
-import "./styles/Global.css";
-import "./styles/Colors.css";
+import "./styles/global.css";
+import "./styles/colors.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
